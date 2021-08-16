03845c86e83f16474d40d2f2a313adcf39f994ac
Domantas Kelpšas <domantas.kelpsas@gmail.com>
2021-08-16 16:30:26 +0300

    Add push notifications

--- love-letter-2/app/src/main/java/com/raywenderlich/android/loveletter/fragment/CreateLetterFragment.kt
+++ love-letter-2/app/src/main/java/com/raywenderlich/android/loveletter/fragment/CreateLetterFragment.kt
@@ -36,6 +36,8 @@ import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.navGraphViewModels
 import com.raywenderlich.android.loveletter.R
 import com.raywenderlich.android.loveletter.databinding.FragmentCreateLetterBinding
 import com.raywenderlich.android.loveletter.extension.Event
@@ -44,54 +46,55 @@ import com.raywenderlich.android.loveletter.viewmodel.LettersViewModel
 
 class CreateLetterFragment : Fragment() {
 
-  private val lettersViewModel: LettersViewModel? = null
+    private val lettersViewModel: LettersViewModel by navGraphViewModels(R.id.nav_graph)
 
-  override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View? {
-    setHasOptionsMenu(true)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        setHasOptionsMenu(true)
 
-    val binding: FragmentCreateLetterBinding = DataBindingUtil.inflate(
-      inflater, R.layout.fragment_create_letter, container, false
-    )
-    binding.viewModel = lettersViewModel
-    return binding.root
-  }
-
-  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-    super.onViewCreated(view, savedInstanceState)
-
-    lettersViewModel?.toastLiveData?.observe(this, Observer<Event<String>> { it ->
-      it.getContentIfNotHandled()?.let {
-        Toast.makeText(this@CreateLetterFragment.context, it, Toast.LENGTH_LONG).show()
-      }
-    })
-  }
-
-  override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-    inflater.inflate(R.menu.create_letter, menu)
-  }
-
-  override fun onOptionsItemSelected(item: MenuItem): Boolean {
-    when (item.itemId) {
-      R.id.action_send -> handleSend { lettersViewModel?.sendLetterWithDeeplink() }
-
-      R.id.action_push -> handleSend { lettersViewModel?.sendPushNotification() }
+        val binding: FragmentCreateLetterBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_create_letter, container, false
+        )
+        binding.viewModel = lettersViewModel
+        return binding.root
     }
-    return super.onOptionsItemSelected(item)
-  }
 
-  private fun handleSend(toSend: () -> Unit) {
-    if (lettersViewModel != null && lettersViewModel.hasFullProfile()) {
-      toSend()
-      // TODO: navigate to sent fragment
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    } else {
-      // TODO: navigate to edit profile fragment
-
+        lettersViewModel?.toastLiveData?.observe(this, Observer<Event<String>> { it ->
+            it.getContentIfNotHandled()?.let {
+                Toast.makeText(this@CreateLetterFragment.context, it, Toast.LENGTH_LONG).show()
+            }
+        })
     }
-    hideKeyboard()
-  }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.create_letter, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_send -> handleSend { lettersViewModel?.sendLetterWithDeeplink() }
+
+            R.id.action_push -> handleSend { lettersViewModel?.sendPushNotification() }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun handleSend(toSend: () -> Unit) {
+        if (lettersViewModel != null && lettersViewModel.hasFullProfile()) {
+            toSend()
+            findNavController().popBackStack(R.id.inboxFragment, false)
+            //findNavController().navigate(R.id.editProfileFragment)
+
+        } else {
+            findNavController().navigate(R.id.editProfileFragment)
+
+        }
+        hideKeyboard()
+    }
 }
